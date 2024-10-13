26132130da740cd4b934460d682597f73b0f8645
Fabio Joaquim <rm551542@fiap.com.br>
2024-10-13 18:02:41 -0300

    Mensagem do commit

--- ProjetoSIP/SistemaIluminacaoPublica/obj/Debug/net8.0/SistemaIluminacaoPublica.AssemblyInfo.cs
+++ ProjetoSIP/SistemaIluminacaoPublica/obj/Debug/net8.0/SistemaIluminacaoPublica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaIluminacaoPublica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c90d82d0dbf4e47f0ca39a44484a5c4e867852f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaIluminacaoPublica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaIluminacaoPublica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
